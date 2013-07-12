7f4750de0855f2380df4d2ae30a3240684433dbb
ceg <max_weisznor@web.de>
2013-07-12 15:29:24 +0000

    Changed web.js  so it reads the content of the added file 'index.html' and show it at the web page

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs  =  require('fs')
 var app = express.createServer(express.logger());
+var bufferIndexHtml = fs.readFileSync("index.html")
+var stringOfBuffer  = bufferIndexHtml.toString("utf-8")
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(stringOfBuffer);
 });
 
 var port = process.env.PORT || 5000;
